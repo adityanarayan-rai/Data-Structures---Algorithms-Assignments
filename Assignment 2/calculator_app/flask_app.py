8deb30f146957f93c34b1d0d6191525cac6e1895
Aditya Narayan Rai <88291361+adityanarayan-rai@users.noreply.github.com>
2024-06-19 15:11:05 +0200

    Add files via upload

---
+++ Assignment 2/calculator_app/flask_app.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template, request
+
+from helper import perform_calculation, convert_to_float
+
+from circle import Circle
+
+app = Flask(__name__)  # create the instance of the flask class
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/calculate', methods=['GET', 'POST'])  # associating the GET and POST method with this route
+def calculate():
+    if request.method == 'POST':
+        # using the request method from flask to request the values that were sent to the server through the POST method
+        value1 = request.form['value1']
+        value2 = request.form['value2']
+        operation = str(request.form['operation'])
+
+        # make sure the input is one of the allowed inputs (not absolutely necessary in the drop-down case)
+        if operation not in ['add', 'subtract', 'divide', 'multiply']:
+            return render_template('calculator.html',
+                                   printed_result='Operation must be one of "add", "subtract", "divide", or "multiply".')
+
+        try:
+            value1 = convert_to_float(value=value1)
+            value2 = convert_to_float(value=value2)
+        except ValueError:
+            return render_template('calculator.html', printed_result="Cannot perform operation with this input")
+
+        try:
+            result = perform_calculation(value1=value1, value2=value2, operation=operation)
+            return render_template('calculator.html', printed_result=str(result))
+
+        except ZeroDivisionError:
+            return render_template('calculator.html', printed_result="You cannot divide by zero")
+
+    return render_template('calculator.html')
+
+
+@app.route('/circle', methods=['GET', 'POST'])
+def circle_calculations():
+    if request.method == 'POST':
+        radius = request.form['radius']
+        calculation = request.form['calculation']
+
+        if calculation not in ['area', 'perimeter']:
+            return render_template('circle_calculator.html', result="Calculation must be either 'area' or 'perimeter'.")
+
+        try:
+            radius = float(radius)
+        except ValueError:
+            return render_template('circle_calculator.html', result="Radius must be a valid number.")
+
+        circle = Circle(radius)
+
+        if calculation == 'area':
+            result = circle.area()
+        else:
+            result = circle.perimeter()
+
+        return render_template('circle_calculator.html', result=f"The {calculation} of the circle with radius {radius} is {result}.")
+    return render_template('circle_calculator.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
